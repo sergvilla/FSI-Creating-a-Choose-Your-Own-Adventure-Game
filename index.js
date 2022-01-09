040ccc364cf16d5dd96a4572006fae62b3df25a9
sergvilla <sergyvill@gmail.com>
2022-01-09 12:39:11 -0500

    commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 let firstAnswer  = window.prompt('Do you head left or right?')
-console.log(firstAnswer ==='left')
+if(firstAnswer === 'left'){
+    let secondAnswer = window.prompt(`You come across a stray cat. 
+It scampers off down a small hole, just large enough for you to crawl through. 
+Do you follow it, or continue on your path?`)
+
+} else if(firstAnswer === 'right') {
+    let secondAnswer = window.prompt(`You come across a snoring dragon. 
+On the other side of him, you see a shiny chest of treasure. Another path would 
+lead you away from the dragon altogether. Which path do you take?`)
+
+}
