f5105264d8e7ccb495b82e8ce5a6e7d965a454a3
sergvilla <sergyvill@gmail.com>
2022-01-09 13:30:43 -0500

    commit

--- index.js
+++ index.js
@@ -5,21 +5,23 @@ It scampers off down a small hole, just large enough for you to crawl through.
 Do you follow it, or continue on your path?`)
 if (secondAnswer ==='follow') {
     let thirdAnswer = window.prompt('You follow the to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying,but you wonder if you should alert the world to this magical safe haven.') }
+    
     if (thirdAnswer === 'stay') {
       window.alert('You live happily amongst the cats for the rest of your days.')
     } else if (thirdAnswer === 'spread the world') {
         window.alert('After leaving the cat colony, you are never able to find it again;without proof, no one believes your story,which passes into legend nonetheless.')
     }
+
 } else if (secondAnswer === 'continue') {
     let thirdAnswer = window.prompt('You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?') 
-} if (thirdAnswer === 'Ladder') {
+if (thirdAnswer === 'Ladder') {
     window.alert('After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.')
 } else if (thirdAnswer === 'staircase') {
     window.alert('After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.')
 }
-}
 
-} else if (firstAnswer === 'right') {
+
+}else if (firstAnswer === 'right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
 On the other side of him, you see a shiny chest of treasure. Another path would 
 lead you away from the dragon altogether. Which path do you take?`)
